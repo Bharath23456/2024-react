b89054348e048cdf33080befefe0c729374907b3
K R BHARATH <bharath899090@gmail.com>
2025-06-18 10:08:47 +0530

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,8 @@ const Login =()=>{
        <div className="login">
         <h2>Login</h2><br></br>
         <form>
-          Email:  <input type="email" placeholder="email" /><br></br>
-         Password:   <input type="password" placeholder="password" /><br></br>
+          Email:  <input type="email" placeholder="Enter your email" /><br></br>
+         Password:   <input type="password" placeholder=" Enter your password" /><br></br>
             <button type="submit">Login</button>
             <a href="">forget password</a>
         </form>
