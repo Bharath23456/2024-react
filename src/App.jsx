22114dd77aa6794ec752de99dae4bc98932e877d
K R BHARATH <bharath899090@gmail.com>
2025-06-18 17:23:51 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import Table from "./components/Table"
+const App = () => {
+  const [count, setCount] = useState(0);
 
-import Navigation from "./components/Navigation";
-import Table from "./components/Table";
-const App=()=>{
- 
-  return <div className="container">
-  <Navigation />
-  <Table name="Bharath" />
-  <Table name="Manjula" />
-  </div>
-}
+  const data1 = [
+    {
+      name: "gmu",
+      value: 1,
+    },
+    {
+      name: "jain",
+      value: 2,
+    },
+    {
+      name: "biet",
+      value: 3,
+    },
+    {
+      name: "davan",
+      value: 4,
+    },
+  ];
+
+    
+  return (
+    <div className="container">
+      <Table data={data1} c={count} setFun={setCount} />
+      <button onClick={() => setCount(count + 1)}>Increment Counter</button>
+    </div>
+  );
+};
+
 export default App;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-
 const Table = (props) => {
+  const { data, c, setFun } = props;
+  let newData = [];
+  if (c < data.length) {
+    newData = [data[c]];
+  }
   return (
-   <h1>HEllo ,{props.name}</h1>
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {newData.map((item) => {
+            return (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.value}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={() => setFun(c - 1)}>Decrement Counter</button>
+    </>
+  );
+};
 
-  )
-}
-
-export default Table
+export default Table;
